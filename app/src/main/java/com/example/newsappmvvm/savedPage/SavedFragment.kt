727bf8e5e21de68766427ddd43a1bde69c46990a
Vaibhav2002 <vaibhavsam2511@gmail.com>
2020-11-18 20:50:15 +0530

    added functionality to swipe in left or right direction to delete any saved news

--- app/src/main/java/com/example/newsappmvvm/savedPage/SavedFragment.kt
+++ app/src/main/java/com/example/newsappmvvm/savedPage/SavedFragment.kt
@@ -8,10 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.example.newsappmvvm.MainActivity
 import com.example.newsappmvvm.MainViewModel
 import com.example.newsappmvvm.databinding.FragmentSavedBinding
 import com.example.newsappmvvm.modelData.Article
+import com.google.android.material.snackbar.Snackbar
 
 
 class SavedFragment : Fragment(), onItemClickListener {
@@ -27,13 +30,36 @@ class SavedFragment : Fragment(), onItemClickListener {
     ): View? {
         binding = FragmentSavedBinding.inflate(inflater, container, false)
         viewModel = (activity as MainActivity).viewModel
-        Log.i("SavedFragVM",viewModel.toString())
-        savedAdapter= SavedNewsAdapter(this)
+        Log.i("SavedFragVM", viewModel.toString())
+        savedAdapter = SavedNewsAdapter(this)
         binding.recyclerView.adapter = savedAdapter
         viewModel.getAllSavedNews().observe(viewLifecycleOwner, Observer {
-            Log.i("list",it.toString())
+            Log.i("list", it.toString())
             savedAdapter.submitList(it)
         })
+
+        val itemTouchHelper = object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val article = savedAdapter.currentList[viewHolder.absoluteAdapterPosition]
+                viewModel.deleteNews(article)
+                Snackbar.make(binding.root, "News deleted", Snackbar.LENGTH_LONG).apply {
+                    setAction("Undo") {
+                        viewModel.saveNewsIntoDB(article)
+                    }
+                    show()
+                }
+            }
+        }
+        ItemTouchHelper(itemTouchHelper).attachToRecyclerView(binding.recyclerView)
         return binding.root
     }
 
